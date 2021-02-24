0bc3b0d2d7fb7e38fff3eed05d4ad6a7153512b0
Navaneeth Suresh <navaneeths1998@gmail.com>
2021-02-24 18:04:03 +0530

    add mca-proclaimed-offenders-ind.py

---
+++ unscrapulous/mca-proclaimed-offenders-ind.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from utils import *
+
+PARENT_SOURCE = 'http://www.mca.gov.in'
+SOURCE = 'http://www.mca.gov.in/MinistryV2/proclaimedoffenders.html'
+OUTPUT_DIR = os.path.join(os.getcwd(), 'files')
+OUTPUT_FILE = 'mca-proclaimed-offenders-ind.csv'
+
+def main():
+    create_dir(OUTPUT_DIR)
+    soup = get_soup(SOURCE)
+    links = [PARENT_SOURCE + a['href'] for a in soup.find_all('a', class_='links_black_EES')]
+    file_urls = []
+    for link in links:
+        soup = get_soup(link)
+        for a in soup.find_all('a', class_='links_black_EES'):
+            file_urls.append(PARENT_SOURCE + a['href'])
+
+    filenames = download_files(file_urls=file_urls, output_dir=OUTPUT_DIR).keys()
+    # TODO: convert downloaded files to csv and write a global csv
+    # Note that most of the files don't contain data in a readable format
+
+if __name__ == '__main__':
+    main()

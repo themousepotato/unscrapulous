f643a498da2386fbbe7a49719f22061cb8788b86
Navaneeth Suresh <navaneeths1998@gmail.com>
2021-02-24 18:20:49 +0530

    add arbitration-awards-bse.py

---
+++ unscrapulous/arbitration-awards-bse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from utils import *
+
+SOURCE = 'https://www.bseindia.com/investors/ArbitAwards.aspx'
+OUTPUT_DIR = os.path.join(os.getcwd(), 'files')
+OUTPUT_FILE = 'arbitration-awards-bse.csv'
+
+def main():
+    create_dir(OUTPUT_DIR)
+    soup = get_soup(SOURCE)
+    table = get_table(soup, {'id' : 'ContentPlaceHolder1_gvAwardDetails'})
+    convert_into_csv(filenames=[OUTPUT_FILE], output_dir=OUTPUT_DIR, table=table)
+
+    # TODO: download data from range of 1 year and paginate
+    alias = {
+        'Name': 'Name of Member',
+        'AddedDate': 'Date Of Issue'
+    }
+    write_global_csv(filename=os.path.join(OUTPUT_DIR, OUTPUT_FILE), source=SOURCE, alias=alias)
+
+
+if __name__ == '__main__':
+    main()
